2e1e46fb0eb75b07c4deb51bbc5df9310abe5d46
leorog <leo.rog@gmail.com>
2021-03-13 15:13:19 -0300

    gen project structure
    command to reproduce
    
    mix phx.new bluecode-test --module Checksum --app checksum --no-ecto --no-html --no-dashboard --no-webpack --no-gettext

---
+++ lib/checksum_web/endpoint.ex
@@ -0,0 +1,19 @@
+defmodule ChecksumWeb.Endpoint do
+  use Phoenix.Endpoint, otp_app: :checksum
+
+  if code_reloading? do
+    plug Phoenix.CodeReloader
+  end
+
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :json],
+    pass: ["*/*"],
+    json_decoder: Phoenix.json_library()
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug ChecksumWeb.Router
+end
---
+++ lib/checksum_web/router.ex
@@ -0,0 +1,11 @@
+defmodule ChecksumWeb.Router do
+  use ChecksumWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/v1", ChecksumWeb do
+    pipe_through :api
+  end
+end
