8232a9c00a34307ae2a3de681e156fadd0a2056b
leorog <leo.rog@gmail.com>
2021-03-14 11:24:32 -0300

    prepare release config

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :checksum, ChecksumWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [port: 4000],
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
