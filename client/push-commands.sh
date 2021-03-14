01ffb67658e1c26b596adec1221fa015a16c79b9
leorog <leo.rog@gmail.com>
2021-03-14 12:13:18 -0300

    add client

---
+++ client/push-commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o pipefail
+
+result=$(cat client/input.txt \
+             | xargs -I {} curl -s http://localhost:4000/v1/number -H 'content-type: application/json' -d '{"command": "{}"}' \
+             | sed 's/[^0-9]*//g')
+
+echo "checksum result: $result"
